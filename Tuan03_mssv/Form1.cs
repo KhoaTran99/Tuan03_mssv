d2cfa707285ad3028c25b04903fb3b273a9726b8
Administrator <Administrator@E03S0260>
2025-10-01 09:39:32 +0700

    Add project files.

---
+++ Tuan03_mssv/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tuan03_mssv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trợGiúpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var data = DateTime.Now.ToString("dd/MM/yyyy");
+            var time = DateTime.Now.ToString("hh:mm:ss");
+
+            toolStripStatusLabel1.Text = $"Hôm nay là ngày: {data} - {time}";
+        }
+
+        private void mởFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog(); // khai báo đối tượng mở file.
+
+            openFileDialog.Filter = "File name|*.mp4"; ;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                axWindowsMediaPlayer1.URL = openFileDialog.FileName;
+
+            }
+
+        }
+    }
+}
